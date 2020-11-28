6f71f2401f55bd3825b8d6e3620629452a76c9a7
Oscar N <PaintNinja@users.noreply.github.com>
2020-11-28 14:19:37 +0000

    Initial template release

---
+++ snowpack.config.js
@@ -0,0 +1,31 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-babel',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
+    ['@snowpack/plugin-optimize', { "preloadModules": true }]
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
